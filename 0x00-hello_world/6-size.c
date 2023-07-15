62ea8a4baca563cd8ce6e0b7ce8bcb4dc5e33a37
bensolve <benkissa10@gmail.com>
2023-07-15 06:47:41 -0700

    check size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - to print the sizes of the various types on the pc
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+	int number;
+	long int largeNumber;
+	long long int veryLargeNumber;
+	float decimalNumber;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(letter));
+	printf("Size of an int: %zu byte(s)\n", sizeof(number));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(largeNumber));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(veryLargeNumber));
+	printf("Size of a float: %zu byte(s)\n", sizeof(decimalNumber);
+	return (0);
