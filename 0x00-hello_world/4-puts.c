54194e3655de3a0bfd7365fb4822412cf989567b
bensolve <benkissa10@gmail.com>
2023-07-14 20:52:51 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - to print text followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char text[]  = "Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
