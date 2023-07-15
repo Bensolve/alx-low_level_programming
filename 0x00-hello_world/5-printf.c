8f0fdeb169c2f4f9440a5069a7931241c0b780de
bensolve <benkissa10@gmail.com>
2023-07-14 21:38:29 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - to print text followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char text[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", text);
+	return (0);
+}
