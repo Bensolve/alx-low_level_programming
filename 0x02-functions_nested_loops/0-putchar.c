c9539d4d16233101091f9b81e1556bcabbca82e7
bensolve <benkissa10@gmail.com>
2023-07-18 05:52:02 -0700

    correct output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@ int main(void)
 	int start;
 	char text[] = "_putchar";
 
-	for (start = 0; start <= 8; ++start)
+	for (start = 0; start < 8; ++start)
 		putchar(text[start]);
 	putchar('\n');
 	return (0);
