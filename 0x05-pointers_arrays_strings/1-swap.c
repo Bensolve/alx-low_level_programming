09944a56ddb6faff448030ac83e320d1391809d1
bensolve <benkissa10@gmail.com>
2023-07-24 22:42:23 -0700

    swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int -  swaps the values of two integers.
+ * @a: first character to be swapped.
+ * @b: second character to be swapped.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
