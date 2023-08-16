ca045b9d2ccd0778acd901b6ce0a9adfa4c59fa1
bensolve <benkissa10@gmail.com>
2023-08-15 17:20:58 -0700

    un love

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - display struct dog variables
+ * @d: collection of struct dog variables
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+	}
+	if (d->name == NULL)
+		printf("Name: (nil\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == -1)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %.1f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+
+
+}
