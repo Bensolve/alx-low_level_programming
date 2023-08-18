bdb50e4116e398ec8a1de447b29f96bb2171279d
bensolve <benkissa10@gmail.com>
2023-08-18 01:37:13 -0700

    print all numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - display of numbers in a line
+ * @separator: used to seperate numebers on the line
+ * @n: number of arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, x;
+	va_list nums;
+
+	if (separator != NULL)
+	{
+		va_start(nums, n);
+		for (i = 0; i < n; i++)
+		{
+			x = va_arg(nums, int);
+			printf("%d", x);
+			if (i < (n - 1))
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(nums);
+	}
+}
