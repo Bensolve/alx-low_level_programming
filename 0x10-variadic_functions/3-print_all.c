c45eef21010fb8c940b5cb85d8f326a4340cd60e
bensolve <benkissa10@gmail.com>
2023-08-18 02:46:36 -0700

    print texts

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print the texts
+ * @separator: as the name suggest
+ * @n: number of texts passed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	var_list = words;
+
+	if (separator != NULL)
+	{
+		var_start(words, n);
+		for (i = 0; i < n; i++)
+		{
+			const char *word = va_arg(words, const *char);
+
+			if (word == NULL)
+				printf("(nil)");
+			else
+				printf("%s", word);
+			if (i < (n - 1))
+				printf("%s", separator);
+		}
+		va_end(words);
+		printf("\n");
+}
